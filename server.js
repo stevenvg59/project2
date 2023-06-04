9711bbfd5fa84af957cf6d485abc538b3be5c96d
dstevenvg <stevensvg59@gmail.com>
2023-06-03 19:36:17 -0500

    Setting the callback URL and the auth router

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+require('dotenv').config();
 
 const port = process.env.PORT || 8080;
+
+const { auth, requiresAuth } = require('express-openid-connect');
 const app = express();
+
+
+const config = {
+        authRequired: false,
+        auth0Logout: true,
+        baseURL: process.env.BASE_URL,
+        clientID: process.env.CLIENT_ID,
+        issuerBaseURL: process.env.ISSUER_BASE_URL,
+        secret: process.env.SECRET
+      };
+
+
+// The `auth` router attaches /login, /logout
+// and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.oidc.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(
+    req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out'
+  )
+});
+
+// The /profile route will show the user profile as JSON
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user, null, 2));
+});
+
 
 app
         .use(bodyParser.json())
